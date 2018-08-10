a77791bf22697fbbff1cf1ae711fb44da854919f
pavankomandur <pavan.komandur@gmail.com>
2018-08-09 23:09:01 -0400

    Initial Checkin

---
+++ src/main/java/com/personal/replenish/replenishtask/ReplenishtaskApplication.java
@@ -0,0 +1,12 @@
+package com.personal.replenish.replenishtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplenishtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplenishtaskApplication.class, args);
+	}
+}
